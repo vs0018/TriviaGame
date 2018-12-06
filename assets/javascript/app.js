c01af907de7e652a0a6cc7f37cf10d053e95984e
Valerie S <43964677+vs0018@users.noreply.github.com>
2018-12-06 12:58:15 -0500

    create css reset, index shell, pseudocode for js

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+//when player clicks a button to start the game, a question is randomly selected from the trivia array
+
+//display it on the page & start countdown
+
+
+
+
+
+//player selects an answer, answer is stored
+
+
+//run function to generate next question is called
+
+
+
+
+
+
+
+
+//when time runs out, check player's answers against the actual answers
+
+//display total correct
