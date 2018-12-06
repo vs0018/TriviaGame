bfe24422cc38723b85fed159066b632797e8aaf6
Valerie S <43964677+vs0018@users.noreply.github.com>
2018-12-06 13:51:10 -0500

    refine pseudocode

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,16 @@
+//variables needed:
+    //game object
+        //question objects (4)
+            //answer property: boolean (true/false) value
+        //player response array (booleans)
+
+
+
+
+
 //when player clicks a button to start the game, a question is randomly selected from the trivia array
+
+
 
 //display it on the page & start countdown
 
@@ -6,7 +18,17 @@
 
 
 
-//player selects an answer, answer is stored
+
+
+
+
+//player selects an answer
+
+
+//player's answer is compared to the boolean value of the current question object
+
+
+//result stored as boolean in its own array (response array)
 
 
 //run function to generate next question is called
@@ -17,7 +39,7 @@
 
 
 
+//when time runs out, use Array.filter() method to determine # of the player's answers that evaluated to 'true'
 
-//when time runs out, check player's answers against the actual answers
 
-//display total correct
+//display total correct (# of trues/total number of questions)
